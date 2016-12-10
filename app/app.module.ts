6215bb99e5deb082ce41e1b1cc8eeb4829820bcb
Mark <mark.lishman@gmail.com>
2016-12-10 09:33:51 +0000

    Upgraded to Angular 2.3.0

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { ColorValueComponent } from "./color-value.component";
-import { FormsModule } from "@angular/forms";
 import { ColorBoxComponent } from "./color-box.component";
 
 @NgModule({
